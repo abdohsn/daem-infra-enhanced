a913087e474ed48ac4c89dfb1b8c9cab85619db5
Abdelrahaman-Hassan <abdohsn39@yahoo.com>
2021-08-08 11:36:51 +0200

    fixing the subset name

--- express-load-balancer.tf
+++ express-load-balancer.tf
@@ -2,9 +2,9 @@ resource "aws_alb" "express_load_balancer" {
   name               = "backend-lb-tf" # Naming our load balancer
   load_balancer_type = "application"
   subnets = [ # Referencing the default subnets
-    "${aws_subnet.default_subnet_a.id}",
-    "${aws_subnet.default_subnet_b.id}",
-    "${aws_subnet.default_subnet_c.id}"
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
   ]
   # Referencing the security group
   security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
